9ae5111bd6d5c07a05c89b8592413d39d0f10238
Md Galib Hossain <mdgalib23@gmail.com>
2025-03-07 00:03:53 +0600

    style:auth card wrapper

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from '@/components/auth/login-form'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <LoginForm/>
+  )
+}
+
+export default Login
---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,12 @@
+
+const DashboardLayout = ({children}:{children: React.ReactNode}) => {
+  return (
+    <div>Dashboard layout
+
+
+        {children}
+    </div>
+  )
+}
+
+export default DashboardLayout
