c1f4c62d66890ae987051892ceb1ec69c717af4e
Md Galib Hossain <mdgalib23@gmail.com>
2025-03-07 23:44:48 +0600

    feat: middleware & login

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,13 +1,33 @@
-"use server"
+"use server";
 
-import { LoginSchema } from "@/schemas"
-import { z } from "zod"
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { LoginSchema } from "@/schemas";
+import { AuthError } from "next-auth";
+import { z } from "zod";
 
-export const login = async(values:z.infer<typeof LoginSchema>)=>{
-    console.log(values)
-    const validatedFields = LoginSchema.safeParse(values)
-    if(!validatedFields.success){
-        return {error:"Invalid fields!"}
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+  console.log(values);
+  const validatedFields = LoginSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
+  const { email, password } = validatedFields.data;
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials!" };
+        default:
+          return { error: "Something went wrong" };
+      }
     }
-    return {success:"Email sent!"}
-}
+    throw error;
+  }
+};
--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
-
+import authConfig from "./auth.config"
+import {PrismaAdapter} from "@auth/prisma-adapter"
+import { db } from "./lib/db"
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+    adapter:PrismaAdapter(db),
+    session: {strategy:"jwt"},
+...authConfig,
 })
